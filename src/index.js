ccda1eaac6b29297820583e8203a74a655668545
ranjayzheng <ranjayzheng@qq.com>
2019-02-05 20:16:36 +0800

    remove fundebug

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // Copyright 2019 ranjayzheng
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-class ErrorMonitor {
-    constructor(options = {}) {
+export default class ErrorMonitor {
+    constructor (options = {}) {
         this.options = options
         this.monitorResult = {}
     }
-    addResult(type, result = {}) {
+    addResult (type, result = {}) {
         result.time = new Date().getTime()
         result.url = window.location.href
         if (this.monitorResult[type]) {
@@ -26,7 +26,7 @@ class ErrorMonitor {
             this.monitorResult[type] = [result]
         }
     }
-    init() {
+    init () {
         //  重写window.onerror
         const oldWindowOnerror = window.onerror
         window.onerror = (message, src, line, column, error) => {
@@ -92,5 +92,3 @@ class ErrorMonitor {
         // XMLHttpRequest
     }
 }
-
-export default ErrorMonitor
